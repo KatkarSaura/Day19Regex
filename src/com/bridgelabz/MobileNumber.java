5ab73f044878586d4b5910b654b73c6a653b6c3d
Pooja <pooja.katkr@gmail.com>
2022-08-30 17:51:19 +0530

    User need enter first valid name

---
+++ src/com/bridgelabz/MobileNumber.java
@@ -0,0 +1,26 @@
+package com.bridgelabz;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MobileNumber {
+
+
+    public static boolean isMobileNumber(String mobileNumber) {
+
+        Pattern pattern = Pattern.compile("^[0-9]{1,2}\\s[7-9][0-9]{9}$");
+
+        if (mobileNumber == null) {
+            return false;
+        }
+        Matcher matcher = pattern.matcher(mobileNumber);
+
+        return matcher.matches();
+
+    }
+    public static void main(String[] args) {
+        String num = "91 9876543210";
+        System.out.println(isMobileNumber(num));
+    }
+}
+
+
